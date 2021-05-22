1953e8e430bd5abed8f80cc7bcabb0e36ae219d1
juancastroTest <juanchicastro02@gmail.com>
2021-05-22 19:01:03 -0300

    animated

---
+++ animar.js
@@ -0,0 +1,30 @@
+// let animado = document.querySelectorAll(".animado");
+
+
+
+// function mostrarScroll() {
+
+//     let scrollTop = document.documentElement.scrollTop
+    
+
+
+//     for(let i = 0; i < animado.length; i++ ){
+
+//     console.log(scrollTop)
+
+
+//         let altura = animado[i].offsetTop
+
+//         console.log(altura)
+
+//         if(altura - 200 < scrollTop){
+
+//             animado[i].style.opacity = 1
+
+//         }
+
+//     }
+
+// }
+
+// window.addEventListener('scroll', mostrarScroll());
